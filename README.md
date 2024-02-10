e87692705f9c96007bdcf14f17c038bb6cbba948
AMIdrissi <amidrissi2002@gmail.com>
2024-02-10 15:38:41 +0100

    chatBot backend currently based on OpenAI API and elevenLabsApi

---
+++ README.md
@@ -0,0 +1,18 @@
+
+
+![Video Thumbnail](https://img.youtube.com/vi/EzzcEL_1o9o/maxresdefault.jpg)
+
+[Video tutorial](https://youtu.be/EzzcEL_1o9o)
+
+The frontend is [here](https://github.com/wass08/r3f-virtual-girlfriend-frontend).
+
+## Setup
+Create a `.env` file at the root of the repository to add your **OpenAI** and **ElevenLabs API Keys**. Refer to `.env.example` for the environment variable names.
+
+Download the **RhubarbLibrary** binary for your **OS** [here](https://github.com/DanielSWolf/rhubarb-lip-sync/releases) and put it in your `bin` folder. `rhubarb` executable should be accessible through `bin/rhubarb`.
+
+Start the development server with
+```
+yarn
+yarn dev
+```
---
+++ index.js
@@ -0,0 +1,148 @@
+import { exec } from "child_process";
+import cors from "cors";
+import dotenv from "dotenv";
+import voice from "elevenlabs-node";
+import express from "express";
+import { promises as fs } from "fs";
+import OpenAI from "openai";
+dotenv.config();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY || "-", // Your OpenAI API key here, I used "-" to avoid errors when the key is not set but you should not do that
+});
+
+const elevenLabsApiKey = process.env.ELEVEN_LABS_API_KEY;
+// const voiceID = "kgG7dCoKCfLehAPWkJOE";
+const voiceID = "VR6AewLTigWG4xSOukaG";
+// const voiceID = "onwK4e9ZLuTAKqWW03F9";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/voices", async (req, res) => {
+  res.send(await voice.getVoices(elevenLabsApiKey));
+});
+
+const execCommand = (command) => {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) reject(error);
+      resolve(stdout);
+    });
+  });
+};
+
+const lipSyncMessage = async (message) => {
+  const time = new Date().getTime();
+  console.log(`Starting conversion for message ${message}`);
+  await execCommand(
+    `ffmpeg -y -i audios/message_${message}.mp3 audios/message_${message}.wav`
+    // -y to overwrite the file
+  );
+  console.log(`Conversion done in ${new Date().getTime() - time}ms`);
+  await execCommand(
+    `./bin/rhubarb -f json -o audios/message_${message}.json audios/message_${message}.wav -r phonetic`
+  );
+  // -r phonetic is faster but less accurate
+  console.log(`Lip sync done in ${new Date().getTime() - time}ms`);
+};
+
+app.post("/chat", async (req, res) => {
+  const userMessage = req.body.message;
+  console.log(userMessage);
+  if (!userMessage) {
+    res.send({
+      messages: [
+        {
+          text: "Hello and welcome to Open Source Days! I'm your AI Assistant, ready to help.",
+          audio: await audioFileToBase64("audios/intro_0.wav"),
+          lipsync: await readJsonTranscript("audios/intro_0.json"),
+          facialExpression: "smile",
+          animation: "Idle",
+        },
+        {
+          text: " Any questions or information you need, just ask. Enjoy the event!",
+          audio: await audioFileToBase64("audios/intro_1.wav"),
+          lipsync: await readJsonTranscript("audios/intro_1.json"),
+          facialExpression: "smile",
+          animation: "Bow",
+        },
+      ],
+    });
+    return;
+  }
+  if (!elevenLabsApiKey || openai.apiKey === "-") {
+    res.send({
+      messages: [
+        {
+          text: "Please my dear, don't forget to add your API keys!",
+          audio: await audioFileToBase64("audios/api_0.wav"),
+          lipsync: await readJsonTranscript("audios/api_0.json"),
+          facialExpression: "angry",
+          animation: "Idle",
+        },
+      ],
+    });
+    return;
+  }
+
+  const completion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    max_tokens: 1000,
+    temperature: 0.6,
+    messages: [
+      {
+        role: "system",
+        content: `
+        You are a virtual assistant for an event called "open source days".
+        You will always reply with a JSON array of messages. With a maximum of 3 messages.
+        Each message has a text, facialExpression, and animation property.
+        The different facial expressions are: smile, sad, angry, surprised, funnyFace, and default.
+        The different animations are: Idle, Bow, WavingGesture, HandsForward and Angry. 
+        `,
+      },
+      {
+        role: "user",
+        content: userMessage || "Hello",
+      },
+    ],
+  });
+  let messages = JSON.parse(completion.choices[0].message.content);
+  if (messages.messages) {
+    messages = messages.messages; // ChatGPT is not 100% reliable, sometimes it directly returns an array and sometimes a JSON object with a messages property
+  }
+  for (let i = 0; i < messages.length; i++) {
+    const message = messages[i];
+    // generate audio file
+    const fileName = `audios/message_${i}.mp3`; // The name of your audio file
+    const textInput = message.text; // The text you wish to convert to speech
+    await voice.textToSpeech(elevenLabsApiKey, voiceID, fileName, "textInput");
+    // generate lipsync
+    await lipSyncMessage(i);
+    message.audio = await audioFileToBase64(fileName);
+    message.lipsync = await readJsonTranscript(`audios/message_${i}.json`);
+  }
+
+  res.send({ messages });
+});
+
+const readJsonTranscript = async (file) => {
+  const data = await fs.readFile(file, "utf8");
+  return JSON.parse(data);
+};
+
+const audioFileToBase64 = async (file) => {
+  const data = await fs.readFile(file);
+  return data.toString("base64");
+};
+
+app.listen(port, () => {
+  console.log(`ByteBuddy listening on port ${port}`);
+});
+
